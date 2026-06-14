a86c2f1bce1cc92e8b98991763d354df217f7220
Simonas Liolaitis <simonas.liolaitis@gmail.com>
2026-06-14 21:46:57 +0300

    add simple backend and frontend communication example

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    let cancelled = false;
+    fetch("http://localhost:3000")
+      .then((res) => res.json())
+      .then((json) => {
+        if (!cancelled) setData(json);
+      })
+      .catch((err) => console.error(err));
+    return () => {
+      cancelled = true;
+    };
+  }, []);
+
+  console.log(data);
+
   return (
     <>
       <h1>Vite + React</h1>
